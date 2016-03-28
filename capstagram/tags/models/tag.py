d1873b71231928b27635b8efb7f6a2d0f7dc6f97
EunJung-Seo <soudg2@gmail.com>
2016-03-28 20:43:04 +0900

    init tag model

---
+++ capstagram/tags/models/tag.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=20,
+    )
+
+    def __str__(self):
+        return "#{tag_name}".format(
+            tag_name=self.name
+        )
