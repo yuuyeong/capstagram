81bcef865e2d70a1f772998a74bc03ceadf4332d
EunJung-Seo <soudg2@gmail.com>
2016-03-28 15:21:09 +0900

    set up users model

--- capstagram/capstagram/settings.py
+++ capstagram/capstagram/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom Authentication Settings
+AUTH_USER_MODEL = 'users.User'
--- capstagram/users/models/user.py
+++ capstagram/users/models/user.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-	email = models.CharField(
-		max_length=30,
-	)
+    descriptions = models.TextField()
