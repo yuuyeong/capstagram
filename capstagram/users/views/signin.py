ed8c5812dc924ad7d43f0428b456a0c189bd178f
EunJung-Seo <soudg2@gmail.com>
2016-03-29 11:33:36 +0900

    update Post app : add SignIn view and template

--- capstagram/users/views/signin.py
+++ capstagram/users/views/signin.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import login
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 
@@ -7,22 +8,21 @@ class UserSignIn(View):
     def get(self, request):
         return render(
             request,
-            "users/signin.html",
+            "users/login.html",
             context={},
         )
 
     def post(self, request):
-        # inputed_id = request.POST.get("my_id")
-        # inputed_pw = request.POST.get("my_pw")
-        # next_page = request.POST.get("my_url")
+        inputed_id = request.POST.get("my_id")
+        inputed_pw = request.POST.get("my_pw")
+        next_page = reverse("home")
 
-        # user = authenticate(
-        #     username=inputed_id,
-        #     password=inputed_pw,
-        # )
+        user = authenticate(
+            username=inputed_id,
+            password=inputed_pw,
+        )
 
-        # if user:
-        #     login(request, user)
-        #     return redirect(next_page)
-        # return redirect(reverse("siginin") + "?next=" + next_page)
-        pass
+        if user:
+            login(request, user)
+            return redirect(next_page)
+        return redirect(reverse("signin"))
